658be47afe685c860d3e362996af263cc8db38b8
Viktor Denikin <leshden@yandex.ru>
2022-05-23 22:06:09 +0300

    second

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import useFetchJobs from './useFetchJobs';
+import {Container} from 'react-bootstrap';
 
 function App() {
+  const {jobs, loading, error} = useFetchJobs()
   return (
-    <div className="App">
-      Hello World!
-    </div>
+    <Container>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error...</h1>}
+      <h1>{jobs.length}</h1>
+    </Container>
   );
 }
 
